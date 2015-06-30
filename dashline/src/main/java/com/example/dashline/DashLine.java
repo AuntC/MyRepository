085d6d2efee2709a3352d544ec6921ed52a0fe5d
蔡炜鑫 <caiweixin@meizu.com>
2015-06-30 10:49:28 +0800

    [dashline]optimize code

--- dashline/src/main/java/com/example/dashline/DashLine.java
+++ dashline/src/main/java/com/example/dashline/DashLine.java
@@ -11,28 +11,34 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class DashLine extends View {
+    private PathEffect effects = new DashPathEffect(new float[]{20, 10}, 1);//虚线规则，20px实线，10px空白
+    private Paint paint;
+    private int width, height;
+
+    Path path;
 
     public DashLine(Context context, AttributeSet attrs) {
         super(context, attrs);
+        paint = new Paint();
+        path = new Path();
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);//线条颜色
+        paint.setAlpha(0x26);//线条透明度
+        paint.setPathEffect(effects);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int width = getWidth();
-        int height = this.getHeight();
+        width = getWidth();
+        height = getHeight();
 
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.BLACK);//线条颜色
-        paint.setAlpha(0x26);//线条透明度
         paint.setStrokeWidth(width);//线条宽度
-        Path path = new Path();
         path.moveTo(width / 2, 0);//线条起点
         path.lineTo(width / 2, height);//线条终点
-        PathEffect effects = new DashPathEffect(new float[]{20, 10}, 1);//虚线规则，20px实线，10px空白
-        paint.setPathEffect(effects);
+
         canvas.drawPath(path, paint);
     }
 }
